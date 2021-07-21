b1338287bae2b0f2784283e1e138bef9f330b7f7
Beth <bethm.meeker@gmail.com>
2021-07-21 11:22:25 -0600

    Add tests for form inputs

--- cypress/integration/reservations_spec.js
+++ cypress/integration/reservations_spec.js
@@ -1,7 +1,21 @@
 describe('Reservations display', () => {
 
-  it('Should confirm true to be true', () => {
-    expect(true).to.eq(true);
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/');
+  })
+
+  it('Should navigate to the application at http://localhost:3000/', () => {
+    cy.url().should('include', 'localhost:3000');
+  })
+
+  it('Should display the title of the application', () => {
+    cy.get('h1').contains('Turing Cafe Reservations');
+  })
+
+  it('Should display input for name, date, time, and number', () => {
+    cy.get('form input').should('have.attr', 'name')
+    cy.get('form input').should('have.attr', 'placeholder')
+    cy.get('form input').should('not.have.value', 'Beth')
   })
 
 
