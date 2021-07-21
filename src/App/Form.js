dd7b2b7e3156ba8ed460bee13dcaaeecedde3041
Beth <bethm.meeker@gmail.com>
2021-07-21 10:43:10 -0600

    Add Form state keys for input fields and add handleChange onChange. Confirm state is updating with input

--- src/App/Form.js
+++ src/App/Form.js
@@ -5,18 +5,25 @@ class Form extends Component {
   constructor() {
     super();
     this.state ={
-
+      name: '',
+      date: '',
+      time: '',
+      number: ''
     }
+  }
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value })
   }
 
   render() {
     return (
       <form>
-        <input type='text' placeholder='Name' required />
-        <input type='text' placeholder='Date(mm/dd)' required />
-        <input type='text'  placeholder='Time' required />
-        <input type='number' placeholder='Number of guests' required />
-        <button type='submit'>Make Reservation</button>
+        <input type='text' placeholder='Name' name='name' onChange={(e) => this.handleChange(e)} required />
+        <input type='text' placeholder='Date(mm/dd)' name='date' onChange={(e) => this.handleChange(e)} required />
+        <input type='text'  placeholder='Time' name='time' onChange={(e) => this.handleChange(e)} required />
+        <input type='number' placeholder='Number of guests' onChange={(e) => this.handleChange(e)} name='number' required />
+        <button type='submit' className='book-btn'>Make Reservation</button>
       </form>
     );
   }
